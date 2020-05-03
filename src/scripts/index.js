48ff4f9bbfe35fb01922d4604acafdc46580af6b
Aamod94-coder <60564399+Aamod94-coder@users.noreply.github.com>
2020-05-03 22:15:47 +0530

    new

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,76 +1,47 @@
-document.querySelector("#toggle_action").addEventListener('change',toggle_func)
-
-function toggle_func(e){
-  if (e.target.checked)
-   {
-    document.documentElement.setAttribute('data-theme', 'lite');
-    document.querySelector(".toggletxt").innerHTML="Toggle to Dark Mode";
-    }
-else
-   {
-    document.documentElement.setAttribute('data-theme', 'dark');
-    document.querySelector(".toggletxt").innerHTML="Toggle to Lite Mode";
-   }   
-}
-
-//Api-Key//
-const apikey="02ae867dcdf14639a290bc6f442307c9";
-var article_area=document.getElementById("news-articles");
-//Function to have formatted NEWS//
-function getNews(news){
-  let output="";
-  if(news.totalResults>0){
-    news.articles.forEach(ind=>{
-      output+= 
-        ` <section class="container">
-          <li class="article"><a class="article-link" href="${ind.url}" target="_blank">
-          <div class="img_area">
-          <img src="${ind.urlToImage}" class="article-img" alt="${ind.title}"></img>
-          </div>
-          <h2 class="article-title">${ind.title}</h2>
-          <p class="article-description">${ind.description || "Description not available"}</p> <br>
-          <span class="article-author">-${ind.author? ind.author: "Anon"}</span><br>
-          </a>
-          </li>
-          </section>
-        `;
+let input = document.querySelector("input");
+let newsarticles = document.getElementById("news-articles");
+//function final is here... contains DOM
+function final(Result) {
+  let found = "";
+  if (Result.length == 0) {
+    Found = `<h2>No article was found based on the search.</h2>`;
+    newsarticles.innerHTML = Found;
+    console.log("else");
+  } else {
+    Result.forEach((article) => {
+      let urlToImage = article.urlToImage;
+      let title = article.title;
+      let description = article.description;
+      let author = article.author;
+      let url1 = article.url;
+      found += `
+      <li class="article">
+    <img src="${urlToImage}" class="article-img" alt="image not found">
+    <div>
+      <h2 class="article-title">${title}</h2>
+      <p class="article-description">${description}</p>
+      <p><span class="article-author"><a class="article-link" href="${url1}">${author}</a></span></p>
+    </div>
+  </li>`;
+      newsarticles.innerHTML = found;
     });
-    article_area.innerHTML=output;
   }
-  else
-  { 
-    article_area.innerHTML='<li class="not-found">No article was found based on the search.</li>';
+}
+async function aamod(url) {
+  let response = await fetch(url);
+  let result = await response.json();
+  let finalResult = result.articles;
+  final(finalResult);
+}
+// when page loads, this lines will execute
+let url = `http://newsapi.org/v2/top-headlines?country=in&apiKey=02ae867dcdf14639a290bc6f442307c9`;
+aamod(url);
+// when we type in search bar and press enter below lines will execute
+input.addEventListener("keydown", (f) => {
+  if (f.keyCode === 13) {
+    console.log(f.target.value);
+    let topic = f.target.value;
+    let url = `http://newsapi.org/v2/everything?q=${topic}&apiKey=02ae867dcdf14639a290bc6f442307c9`;
+    aamod(url);
   }
-};
-// Function to retreive news using Fetch API with Await//
-async function retreive(searchValueText=""){
-
-    article_area.innerHTML='<p class="load">News are Loading...</p>';
-    
-    if(searchValueText!=""){
-      url=`https://newsapi.org/v2/everything?q=${searchValueText}&apiKey=${apikey}`;
-    }
-    else
-    {
-      url=`https://newsapi.org/v2/top-headlines?country=in&apiKey=${apikey}`;
-    }
-    const response=await fetch(url);
-    const result=await response.json();
-    getNews(result);
-}
-//Get text value from Searchbar and pass to retreive function//
-async function searchvalue(e){  
-    if (event.which === 13 || event.keyCode === 13 || event.key === "Enter")
-     {
-      retreive(e.target.value);
-     }
-};
-//Attached Event listener for Searchbar to retreive text from Searchbar//
-function start(){
-  document.getElementById("search").addEventListener('keypress',searchvalue);
-  retreive();
-}
-//Initializing Function//
-(function(){
-  start();}
-)();
+});
